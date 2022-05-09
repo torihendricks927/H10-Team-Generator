76d35a555cb29762545d15f3e6899c9b85dc818f
Tori <torihendricks927@yahoo.com>
2022-05-09 18:21:02 -0400

    adding get methods

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -3,7 +3,7 @@ const Employee = require('../lib/Employee');
 
 describe("Employee", () => {
     describe("Initialization", () => {
-        
+
         it("should instantiate an employee instance", () => {
 
             const employee = new Employee();
@@ -22,7 +22,7 @@ describe("Employee", () => {
 
         it("should set id via constructor arguments", () => {
 
-            const id = 092796;
+            const id = 100;
 
             const employee = new Employee("Tori", id);
 
@@ -33,10 +33,53 @@ describe("Employee", () => {
 
             const email = "torihendricks927@yahoo.com";
 
-            const employee = new Employee("Tori", 092796, email);
+            const employee = new Employee("Tori", 100, email);
 
             expect(employee.email).toBe(email);
         });
+    });
+
+    describe("Getter methods", () => {
+        it("should get name via getName()", () => {
+
+            const name = "Tori";
+
+            const employee = new Employee(name, 100, "torihendricks927@yahoo.com");
+            const employeeName = employee.getName()
+
+            expect(employeeName).toBe(name);
+        });
+
+        it("should get id via getId()", () => {
+
+            const id = 100;
+
+            const employee = new Employee("Tori", id, "torihendricks927@yahoo.com");
+            const employeeId = employee.getId()
+
+            expect(employeeId).toBe(id);
+        });
+        
+        it("should get email via getEmail()", () => {
+
+            const email = "Tori";
+
+            const employee = new Employee("Tori", 100, email);
+            const employeeEmail = employee.getEmail()
+
+            expect(employeeName).toBe(name);
+        });
+        
+        it("should get Role via getRole()", () => {
+
+            const role = "Employee";
+
+            const employee = new Employee("Tori", 100, "torihendricks927@yahoo.com");
+            const employeeRole = employee.getRole()
+
+            expect(employeeRole).toBe(role);
+        });
+        
     });
 });
 
