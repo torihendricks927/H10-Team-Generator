4d5dae30a9d656a8ba3bf7850db9ee811bd04a78
Tori <torihendricks927@yahoo.com>
2022-05-10 23:19:47 -0400

    added intern questions

--- index.js
+++ index.js
@@ -165,7 +165,71 @@ function addEngineer() {
 
 
 
-function addIntern() {}
+function addIntern() {
+    inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        validate: (answer) => {
+          if (answer !== "") {
+            return true;
+          }
+          return "Please enter at least one character";
+        },
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "what is the id",
+        validate: (answer) => {
+          const pass = answer.match(/^[1-9]\d*$/);
+          if (pass) {
+            if (idArray.includes(answer)) {
+              return "This id is already taken";
+            } else {
+              return true;
+            }
+          }
+          return "Please enter a positive number greater than 0";
+        },
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "what is the email",
+        validate: (answer) => {
+          const pass = answer.match(/\S+@\S+\.\S+/);
+          if (pass) {
+            return true;
+          }
+          return "Please enter a valid email";
+        },
+      },
+      {
+        type: "input",
+        name: "school",
+        message: "what is the school",
+        validate: (answer) => {
+          if (answer !== "") {
+            return true;
+          }
+          return "Please enter at least one character";
+        },
+      },
+    ])
+    .then((answers) => {
+      const intern = new Intern(
+        answers.name,
+        answers.id,
+        answers.email,
+        answers.school
+      );
+      teamMembers.interns.push(intern);
+      idArray.push(answers.id);
+      createTeam();
+    });
+}
 
 
 
