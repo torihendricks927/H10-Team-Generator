f3b3279d5a0c7948a5964678d44a3c53a05b13b1
Tori <torihendricks927@yahoo.com>
2022-05-10 23:26:43 -0400

    final commit

--- src/page.template.js
+++ src/page.template.js
@@ -9,7 +9,7 @@ function renderManager(manager) {
       <ul class="list-group">
           <li class="list-group-item">ID: ${manager.getId()}</li>
           <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
-          <li class="list-group-item">Office number: ${manager.getOfficeNumber()}</li>
+          <li class="list-group-item">Office number: ${manager.getOffice()}</li>
       </ul>
   </div>
 </div>
