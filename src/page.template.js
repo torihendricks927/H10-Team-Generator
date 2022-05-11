a285e059288b9e14bd1967e78d8f234463397ad0
Tori <torihendricks927@yahoo.com>
2022-05-10 23:08:16 -0400

    modify html file temp

--- src/page.template.js
+++ src/page.template.js
@@ -14,7 +14,22 @@ function renderManager(manager) {
     </div>`;
 }
 function renderEngineer(engineers) {
-    return `engineer`;
+    return markup = engineers.map((engineer) => {
+        return `<div class="card employee-card">
+        <div class="card-header">
+            <h2 class="card-title">${engineer.getName()}</h2>
+            <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${engineer.getRole()}</h3>
+        </div>
+        <div class="card-body">
+            <ul class="list-group">
+                <li class="list-group-item">ID: ${engineer.getId()}</li>
+                <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
+                <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.getGithub()}" target="_blank" rel="noopener noreferrer">${engineer.getGithub()}</a></li>
+            </ul>
+        </div>
+    </div>`
+    });
+    return markup.join(",")
 }
 function renderInterns(interns) {
     return `intern`;
