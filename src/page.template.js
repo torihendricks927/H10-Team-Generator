6b110ddad15376d0fed2d0cb73ddc73ba560f8fc
Tori <torihendricks927@yahoo.com>
2022-05-10 23:12:36 -0400

    add intern temp and fixed engineer temp

--- src/page.template.js
+++ src/page.template.js
@@ -1,20 +1,22 @@
 function renderManager(manager) {
     return `
-    <div class="card" style="width: 18rem;">
-        <div class="card-body">
-            <h5 class="card-title">${manager.getName()}</h5>
-            <h6 class="card-subtitle mb-2">${manager.getRole()}</h6>
-        </div>
-  
-        <ul class="list-group list-group-flush">
-            <li class="list-group-item">ID: ${manager.getId()}</li>
-            <li class="list-group-item">Email: ${manager.getEmail()}</li>
-            <li class="list-group-item">Office Number: ${manager.getOffice}</li>
-        </ul>
-    </div>`;
+    <div class="card employee-card">
+  <div class="card-header">
+      <h2 class="card-title">${manager.getName()}</h2>
+      <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
+  </div>
+  <div class="card-body">
+      <ul class="list-group">
+          <li class="list-group-item">ID: ${manager.getId()}</li>
+          <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
+          <li class="list-group-item">Office number: ${manager.getOfficeNumber()}</li>
+      </ul>
+  </div>
+</div>
+  `;
 }
 function renderEngineer(engineers) {
-    return markup = engineers.map((engineer) => {
+    const markup = engineers.map((engineer) => {
         return `<div class="card employee-card">
         <div class="card-header">
             <h2 class="card-title">${engineer.getName()}</h2>
@@ -27,14 +29,31 @@ function renderEngineer(engineers) {
                 <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.getGithub()}" target="_blank" rel="noopener noreferrer">${engineer.getGithub()}</a></li>
             </ul>
         </div>
-    </div>`
+    </div>
+        `;
     });
-    return markup.join(",")
-}
+    return markup.join(",");
+  }
 function renderInterns(interns) {
-    return `intern`;
-}
-
+    const markup = interns.map((intern) => {
+        return `
+            <div class="card employee-card">
+        <div class="card-header">
+            <h2 class="card-title">${intern.getName()}</h2>
+            <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${intern.getRole()}</h3>
+        </div>
+        <div class="card-body">
+            <ul class="list-group">
+                <li class="list-group-item">ID: ${intern.getId()}</li>
+                <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
+                <li class="list-group-item">School: ${intern.getSchool()}</li>
+            </ul>
+        </div>
+    </div>`;
+      });
+      return markup.join(",");
+    }
+    
 function generateTeam(teamMembers) {
     return `
     ${renderManager(teamMembers.manager)}
@@ -46,30 +65,33 @@ function render(teamMembers) {
     return `<!DOCTYPE html>
     <html lang="en">
     <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-        <title>Team Generater</title>
-        <link rel="stylesheet" href="style.css">
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+      <title>My Team</title>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+      <link rel="stylesheet" href="style.css">
+      <script src="https://kit.fontawesome.com/c502137733.js"></script>
     </head>
     <body>
-        <div class="container-fluid">
-            <div class="row">
-            <div class="col-12 jumbotron mb-3 team-heading">
-                <h1 class="text-center">Team Setup</h1>
-            </div>
-        </div>
-        <div class="container">
-            <div class="row">
-                <div class="team-area col-12 d-flex justify-content-center">
-                    ${generateTeam(teamMembers)}
-                </div>
-            </div>
-        </div>
-        
+      <div class="container-fluid">
+          <div class="row">
+              <div class="col-12 jumbotron mb-3 team-heading">
+                  <h1 class="text-center">My Team</h1>
+              </div>
+          </div>
+      </div>
+      <div class="container">
+          <div class="row">
+              <div class="team-area col-12 d-flex justify-content-center">
+                  ${generateTeam(teamMembers)}
+              </div>
+          </div>
+      </div>
     </body>
-    </html>`
+    </html>
+      `
 };
 
 module.exports = render;
